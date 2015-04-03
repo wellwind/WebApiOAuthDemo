8b984429a66acd11f4fdc2a742d56fb311a376c1
wellwind <wellwind@mail2000.com.tw>
2015-04-04 07:32:05 +0800

    Web Api OAuth Demo

--- WebApiOAuthDemo/Properties/AssemblyInfo.cs
+++ WebApiOAuthDemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集 
 // 控制。變更這些屬性值可修改與組件關聯的
 // 資訊。
-[assembly: AssemblyTitle("WebApiDemo")]
+[assembly: AssemblyTitle("WebApiOAuthDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApiDemo")]
+[assembly: AssemblyProduct("WebApiOAuthDemo")]
 [assembly: AssemblyCopyright("Copyright (C)  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
